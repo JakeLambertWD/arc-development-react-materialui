fd93e2a8f7bf574e4e90d88690158424130ca0fd
JakeLambertWD <jakelambert1@hotmail.co.uk>
2022-01-18 16:50:49 +0000

    Footer started w/ grid layout

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(theme => ({
+	footer: {
+		backgroundColor: theme.palette.common.blue
+	}
+}));
+
+const Footer = () => {
+	return <div></div>;
+};
+
+export default Footer;
